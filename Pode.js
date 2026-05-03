c364cd0dcac83e309b18de38c29a86e6c9823357
luisfelipesantoscarvalho485-source <luisfelipesantoscarvalho485@gmail.com>
2026-05-03 03:08:04 -0300

    Update Pode.js

--- Pode.js
+++ Pode.js
@@ -15,7 +15,7 @@ async function gerarCodigo() {
             messages: [
                 {
                     role: "system",
-                    content: "Voce e uma superespecialista em anime..."
+                    content: "Voce e uma superespecialista em anime de qualquer genero, sua reposta e zueira porem com todas as informações que o usuario pedir, se colocar alguma palavra em inglês traduza em portugues brasil, se pergutarem sobre qualquer coisa do anime awards vc sabe de tudo"
                 },
                 {
                     role: "user",
