2fc2aebcb97f72f1e64dc9ea18fcaea32de3a546
luisfelipesantoscarvalho485-source <luisfelipesantoscarvalho485@gmail.com>
2026-05-03 01:33:27 -0300

    Add files via upload

---
+++ Pode.js
@@ -0,0 +1,77 @@
+/* 
+    Variável - Pedacinho de memória
+    que eu posso guardar o que eu quiser
+
+    Função - Pedacinho de código QUE só EXECUTA
+    Quando é chamado
+
+    Algoritmo - Receita do Bolo
+    Lógica de Programação -  Fazer o bolo
+
+    // Algoritmo do nosso sistema
+    // Lógica de programação
+
+    [x] Saber quem é o botão
+    [x] Saber quando o botão foi clicado
+    [x] Saber quem é o textarea  
+    [x] Pegar o que tem dentro dele
+    [x] Enviar para a IA
+    [x] Pegar a resposta da IA e colocar na tela 
+    [ ] Estilizar a resposta     
+
+    // Ir no HTML e pegar o botão
+    // HTML = document (documento)
+    // Selecionar (querySelector)
+    // Quem ? Botão
+    // Apelido para o botão - classes(class) = .
+    fetch - ferramenta do JS para se comunicar com o servidor
+*/
+
+// Descobri que é o botao
+let botao = document.querySelector(".botao-gerar")
+let endereco = "https://api.groq.com/openai/v1/chat/completions"
+// Criei a funcao que será chamada quando clicar 
+// no botao
+
+async function gerarCodigo() {
+
+    let textoUsuario = document.querySelector(".caixa-texto").value
+    let blocoCodigo = document.querySelector(".bloco-codigo")
+    let resultadoCodigo = document.querySelector(".resultado-codigo")
+
+    let resposta = await fetch(endereco, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer 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"
+        },
+        body: JSON.stringify({
+            model: "openai/gpt-oss-120b",
+            messages: [
+                {
+                    role: "system",
+                    content: "Voce e uma superespecialista em anime  QUALQUER TIPO DE ANIME e suas responsta sao brincalhonas porem objetivas e deixe tudo organizado(como diferenciar as letras de titulos e das explicaçoes) caso use ingles ajude na tradução. voce sabe tudo sobre premios indenpendente do ano, voce tem um conhecimento super atualizado sobre animes e seus premios. responda de forma brincalhona e divertida"
+                },
+                {
+                    role: "user",
+                    content: textoUsuario
+                }
+            ]
+        })
+    })
+
+    let dados = await resposta.json()
+    let resultado = dados.choices[0].message.content
+
+    blocoCodigo.textContent = resultado
+    resultadoCodigo.srcdoc = resultado
+
+}
+
+// ficar de olho no botao, quando clicado chamar o gerarCodigo
+botao.addEventListener("click", gerarCodigo)
+
+
+// vizinho curioso (addEventListener)
+// adicionar ouvinte de eventos
+// Evento = clique, digitei...
