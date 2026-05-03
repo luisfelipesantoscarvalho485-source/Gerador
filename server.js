212df3f14accc8badf4e9b0233a9bf7552d99107
luisfelipesantoscarvalho485-source <luisfelipesantoscarvalho485@gmail.com>
2026-05-03 04:24:57 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,11 +8,14 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.get("/", (req, res) => {
+    res.send("API rodando 🚀")
+})
 
 app.post("/gerar", async (req, res) => {
-    console.log("CHEGOU NO BACKEND:", req.body)
-
     try {
+        console.log("REQ RECEBIDO:", req.body)
+
         const resposta = await fetch("https://api.groq.com/openai/v1/chat/completions", {
             method: "POST",
             headers: {
@@ -26,15 +29,19 @@ app.post("/gerar", async (req, res) => {
         })
 
         const dados = await resposta.json()
-        console.log("RESPOSTA DA IA:", dados)
 
-        // 👇 resposta correta pra frontend
         res.json({
-            resposta: dados?.choices?.[0]?.message?.content || "Erro na resposta"
+            resposta: dados?.choices?.[0]?.message?.content || "Sem resposta"
         })
 
-    } catch (erro) {
-        console.log("ERRO:", erro)
-        res.status(500).json({ erro: "Erro no servidor " +port })
+    } catch (err) {
+        console.log("ERRO:", err)
+        res.status(500).json({ erro: "Erro no servidor" })
     }
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log("Servidor rodando na porta " + port)
 })
